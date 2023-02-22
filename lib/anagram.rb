63879376e17db4ebf0bd9bcff9d89b8a1f9f319a
Blake Creasser <blakecreasser@aggienetwork.com>
2023-02-21 22:48:21 -0600

    commit tree 36e767a9f420cc74e876068e248210c515f36c38

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,29 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match arr
+        matched_word = []
+        arr.each do |curr_word|
+            if curr_word.chars.sort == @word.chars.sort
+                matched_word << curr_word
+            #else
+                #puts 'Not a match'
+            end
+        end
+        matched_word
+    end
+
+end
+
+
+#will need to iterate over all the characters in the word and break it into its own array
+#once it is in its own arr we can sort the arr and the starting word arr and compare the two to see if they are the same
+
+#start with getting the starting word destructured how we need it
+#do the same for each word that was passed to the match arr can loop over each word use .each, break the word into its own arr, then compare it to the starting_word_arr, if match shovel it into the match word arr
